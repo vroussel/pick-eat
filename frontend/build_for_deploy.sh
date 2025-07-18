7d53468ffd9adefbef0f51a637ce74b800efb29c
Valentin Roussel <valentin.rssl@gmail.com>
2025-07-18 14:39:35 +0200

    wipe frontend

--- frontend/build_for_deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-npm run build
-mkdir -p ../ansible/to_deploy/frontend
-rm -rf ../ansible/to_deploy/frontend/*
-mv dist/* ../ansible/to_deploy/frontend
-find ../ansible/to_deploy/frontend/ \( -name '*.js' -o -name '*.css' -o -name '*.html' \) -size +1400c -exec sh -c 'brotli --keep --best "$1"; gzip --keep --best "$1"' shell {} \;
