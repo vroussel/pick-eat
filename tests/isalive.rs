a1483f90533cb0302e287a2907bdd11a36ef07d8
Valentin Roussel <valentin.rssl@gmail.com>
2025-08-04 08:29:23 +0200

    bind on random available port in tests

--- tests/isalive.rs
+++ tests/isalive.rs
@@ -1,13 +1,15 @@
+use std::net::SocketAddr;
+
 use pickeat_server::run;
 use tokio::net::TcpListener;
 
 #[tokio::test]
 async fn isalive_works() {
-    spawn_app().await;
+    let bind_addr = spawn_app().await;
     let client = reqwest::Client::new();
 
     let response = client
-        .get("http://127.0.0.1:4242/isalive")
+        .get(format!("http://{bind_addr}/isalive"))
         .send()
         .await
         .expect("Failed to execute request");
@@ -16,9 +18,13 @@ async fn isalive_works() {
     assert_eq!(response.content_length(), Some(0));
 }
 
-async fn spawn_app() {
-    let listener = TcpListener::bind("127.0.0.1:4242")
+async fn spawn_app() -> SocketAddr {
+    let listener = TcpListener::bind("127.0.0.1:0")
         .await
         .expect("Unable to bind on address");
+    let addr = listener
+        .local_addr()
+        .expect("Unable to retrieve server port");
     tokio::spawn(run(listener));
+    addr
 }
