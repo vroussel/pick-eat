82934730cddb62b858c279a006875c9745baa1d0
Valentin Roussel <valentin.rssl@gmail.com>
2025-08-08 17:28:46 +0200

    move integrations test into single file to avoid annoying 'dead code' warning

--- tests/all.rs
+++ tests/all.rs
@@ -1,6 +1,34 @@
-use crate::common::spawn_app;
+use std::net::SocketAddr;
 
-pub(crate) mod common;
+use pickeat_server::run;
+use tokio::net::TcpListener;
+
+#[cfg(test)]
+pub(crate) async fn spawn_app() -> SocketAddr {
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .await
+        .expect("Unable to bind on address");
+    let addr = listener
+        .local_addr()
+        .expect("Unable to retrieve server port");
+    tokio::spawn(run(listener));
+    addr
+}
+
+#[tokio::test]
+async fn isalive_works() {
+    let bind_addr = spawn_app().await;
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get(format!("http://{bind_addr}/isalive"))
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert!(response.status().is_success());
+    assert_eq!(response.content_length(), Some(0));
+}
 
 #[tokio::test]
 async fn add_recipe_returns_200_with_valid_data() {
