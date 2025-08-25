ae24a9aa164385d01f0c3b3eaf89a53afa725fd0
Valentin Roussel <valentin.rssl@gmail.com>
2025-08-25 08:15:58 +0200

    in db image, allow passing passwords directly in env vars or via file paths
    e.g. DB_PICKEAT_PASSWORD or DB_PICKEAT_PASSWORD_FILE

--- db/init/1_create_users.sh
+++ db/init/1_create_users.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
+if [ -n "${DB_PICKEAT_PASSWORD_FILE}" ]; then
+    DB_PICKEAT_PASSWORD="$(cat "${DB_PICKEAT_PASSWORD_FILE}")"
+fi
+if [ -n "${DB_PICKEAT_APP_PASSWORD_FILE}" ]; then
+    DB_PICKEAT_APP_PASSWORD="$(cat "${DB_PICKEAT_APP_PASSWORD_FILE}")"
+fi
+
+if [ -z "$DB_PICKEAT_PASSWORD" ]; then
+    >&2 echo "Missing or invalid env var DB_PICKEAT_PASSWORD[_FILE]"
+fi
+if [ -z "$DB_PICKEAT_APP_PASSWORD" ]; then
+    >&2 echo "Missing or invalid env var DB_PICKEAT_APP_PASSWORD[_FILE]"
+fi
+
 psql << EOF
 CREATE ROLE pickeat WITH
     LOGIN
     CREATEDB
     CREATEROLE
-    PASSWORD '$(cat /run/secrets/db_pickeat_password)';
+    PASSWORD '${DB_PICKEAT_PASSWORD}';
 
 CREATE USER pickeat_app WITH
     LOGIN
-    PASSWORD '$(cat /run/secrets/db_pickeat_app_password)';
+    PASSWORD '${DB_PICKEAT_APP_PASSWORD}';
 
 ALTER DEFAULT PRIVILEGES
     GRANT SELECT, INSERT, UPDATE, DELETE
