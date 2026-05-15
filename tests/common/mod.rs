0ba22be73fc25408e2e22dbbfee6f1b0c06a5240
Valentin Roussel <valentin.rssl@gmail.com>
2026-05-15 11:03:23 +0200

    for tests, use in-doc template instead of inline string with source

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -107,28 +107,26 @@ impl TestApp {
     }
 }
 
+/// ```askama
+/// [http]
+/// ip = "127.0.0.1"
+/// port = 0
+///
+/// [db]
+/// host = "localhost"
+/// port = 5432
+/// name = "{{ test_db_name }}"
+///
+/// [db.app_user]
+/// name = "pickeat_app"
+/// password = "{{ app_user_password }}"
+///
+/// [db.migration_user]
+/// name = "pickeat"
+/// password = "{{ migration_user_password }}"
+/// ```
 #[derive(Template)]
-#[template(
-    ext = "txt",
-    source = r#"
-[http]
-ip = "127.0.0.1"
-port = 0
-
-[db]
-host = "localhost"
-port = 5432
-name = "{{ test_db_name }}"
-
-[db.app_user]
-name = "pickeat_app"
-password = "{{ app_user_password }}"
-
-[db.migration_user]
-name = "pickeat"
-password = "{{ migration_user_password }}"
-    "#
-)]
+#[template(ext = "txt", in_doc = true)]
 struct TestAppConf {
     test_db_name: String,
     app_user_password: String,
