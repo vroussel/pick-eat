74472d86ec28da35d4730a22f62b981efba2fea9
Valentin Roussel <valentin.rssl@gmail.com>
2026-06-06 10:59:46 +0200

    show toast when htmx triggers error event

--- web/assets/app.js
+++ web/assets/app.js
@@ -2,3 +2,19 @@ function reset_input_error(el) {
     el.closest('div').querySelector('.text-error')?.remove()
     el.classList.remove('input-error')
 }
+
+function showToast(message, type='error') {
+    const alert = document.createElement('div');
+    alert.className = `alert alert-${type}`;
+    alert.innerHTML = `${message}`;
+
+    const toast = document.getElementById('toast');
+    if (toast) {
+        toast.appendChild(alert);
+        setTimeout(() => alert.remove(), 3000);
+    }
+}
+
+document.body.addEventListener('htmx:responseError', function(evt) {
+    showToast("Une erreur s'est produite, désolé!<br>Veuillez rééssayer plus tard.")
+});
