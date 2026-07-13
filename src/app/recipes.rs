fa1c51312338191dd2f5dc3c792fa21cb03f2b2b
Valentin Roussel <valentin.rssl@gmail.com>
2026-07-13 07:54:05 +0200

    remove From impl from NewRecipe to Recipe

--- src/app/recipes.rs
+++ src/app/recipes.rs
@@ -3,22 +3,17 @@ use uuid::Uuid;
 use crate::model::{NewRecipe, Recipe};
 use crate::{AppError, AppState, db};
 
-impl From<NewRecipe> for Recipe {
-    fn from(value: NewRecipe) -> Self {
-        Self {
-            id: Uuid::now_v7(),
-            name: value.name,
-            prep_time: value.prep_time,
-            cook_time: value.cook_time,
-        }
-    }
-}
-
 pub async fn create(state: AppState, new_recipe: NewRecipe) -> Result<Recipe, AppError> {
-    let r = new_recipe.into();
-    db::insert_recipe(&state.db_pool, &r).await?;
+    let recipe = Recipe {
+        id: Uuid::now_v7(),
+        name: new_recipe.name,
+        prep_time: new_recipe.prep_time,
+        cook_time: new_recipe.cook_time,
+    };
 
-    Ok(r)
+    db::insert_recipe(&state.db_pool, &recipe).await?;
+
+    Ok(recipe)
 }
 
 pub async fn retrieve(state: AppState, id: &Uuid) -> Result<Option<Recipe>, AppError> {
